8763f2bc947f6695780a1b2519af409abef8ef71
2Kelvin <rocketmancodes02@gmail.com>
2024-07-24 09:26:27 +0300

    run both functions concurrently

--- src/Main.kt
+++ src/Main.kt
@@ -1,20 +1,20 @@
 import kotlinx.coroutines.*
-import kotlin.system.measureTimeMillis
+/**
+ * in a coroutine, when the code suspends(pauses), the coroutines shares the event loop allowing other code to be run
+ * delay is an example of a coroutine suspend function. Other code can run once in delay mode
+ */
 
 fun main() {
-    // measuring the time it takes to run this code
-    val codeTime = measureTimeMillis {
-        // runBlocking is synchronous; waits for all the code in its body to finish before returning to the main function
-        // that means for "Sunny" to be printed, it has to wait for the 1 seconds delay & so does the
-        runBlocking {
-            println("Weather forecast:")
-            printForecast()
-            printTemperature()
-        }
+    // runBlocking is synchronous; waits for all the code in its body to finish before returning to the main function
+    // that means for "Sunny" to be printed, it has to wait for the 1 seconds delay & so does the
+    runBlocking {
+        println("Weather forecast:")
+        // launch coroutine runs two codes at the same time (concurrently)
+        // as printForecast is running so is printTemperature
+        // in real time they are both being delayed in the same exact second
+        launch { printForecast() }
+        launch { printTemperature() }
     }
-
-    //printing the time taken to run the code in seconds
-    println("Code execution time is: ${codeTime / 1000.0} seconds")
 }
 
 // a suspend function is a function that can pause and resume its functionality from where it last left off
@@ -28,8 +28,3 @@ suspend fun printTemperature() {
     delay(1000)
     println("15\u00b0C")
 }
-
-/**
- * in a coroutine, when the code suspends(pauses), the coroutines shares the event loop allowing other code to be run
- * delay is an example of a coroutine suspend function. Other code can run once in delay mode
- */
